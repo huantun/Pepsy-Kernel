fe892fd46893458f8acd55ef33d5c4eed105527c
PsyMan47 <xhonirexhepi@gmail.com>
2018-04-07 16:31:59 +0000

    MIUI Bringup
    *Do not build qcacld-2.0 - Force loading the one existing in MIUI instead
    *Stop building separated dtb image - Append in Image.gz-dtb instead by adjusting dts path

--- AIO-build.sh
+++ AIO-build.sh
@@ -9,11 +9,11 @@ DATE=$(date +"%d%m%Y")
 KERNEL_NAME="Pepsy-Kernel"
 DEVICE="-Xiaomi-MSM8996-"
 VER="-v0.1"
-TYPE="-O-MR1"
+TYPE="-O-MR1-MIUI"
 FINAL_ZIP="$KERNEL_NAME""$DEVICE""$DATE""$TYPE""$VER".zip
 
-rm $ANYKERNEL_DIR/*/zImage $ANYKERNEL_DIR/*/dtb $ANYKERNEL_DIR/*/*.zip
-rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/dt.img $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
+rm $ANYKERNEL_DIR/*/Image.gz-dtb $ANYKERNEL_DIR/*/*.zip $ANYKERNEL_DIR/modules/*.ko
+rm $KERNEL_DIR/arch/arm64/boot/Image.gz $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb
 
 export ARCH=arm64
 export KBUILD_BUILD_USER="Psy_Man"
@@ -27,45 +27,36 @@ make clean && make mrproper
 make gemini_defconfig
 make -j$( nproc --all )
 
-./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/gemini/dtb
-mv $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/gemini/zImage
+mv $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/gemini
 
 export CCACHE_DIR=$CCACHEDIR/capricorn/.ccache
 make clean && make mrproper
 make capricorn_defconfig
 make -j$( nproc --all )
 
-./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/capricorn/dtb
-mv $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/capricorn/zImage
+mv $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/capricorn
 
 export CCACHE_DIR=$CCACHEDIR/natrium/.ccache
 make clean && make mrproper
 make natrium_defconfig
 make -j$( nproc --all )
 
-./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/natrium/dtb
-mv $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/natrium/zImage
+mv $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/natrium
 
 export CCACHE_DIR=$CCACHEDIR/lithium/.ccache
 make clean && make mrproper
 make lithium_defconfig
 make -j$( nproc --all )
 
-./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/lithium/dtb
-mv $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/lithium/zImage
+mv $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/lithium
 
 export CCACHE_DIR=$CCACHEDIR/scorpio/.ccache
 make clean && make mrproper
 make scorpio_defconfig
 make -j$( nproc --all )
 
-./dtbTool -s 2048 -o arch/arm64/boot/dt.img -p scripts/dtc/ arch/arm/boot/dts/qcom/
-mv $KERNEL_DIR/arch/arm64/boot/dt.img $ANYKERNEL_DIR/scorpio/dtb
-mv $KERNEL_DIR/arch/arm64/boot/Image.gz $ANYKERNEL_DIR/scorpio/zImage
+mv $KERNEL_DIR/arch/arm64/boot/Image.gz-dtb $ANYKERNEL_DIR/scorpio
 
+find $KERNEL_DIR -name '*.ko' -exec cp '{}' "$ANYKERNEL_DIR/modules" \;
 cd $ANYKERNEL_DIR
 zip -r9 $FINAL_ZIP * -x *placeholder
